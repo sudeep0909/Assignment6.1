e7f7ce2ba88d689313c2ddd414a2ec9113067686
sudeep0909 <hst.dips@gmail.com>
2018-05-22 16:35:16 +0530

    JavaScript code included in the various locations
     Script files are from - Head section Body section & external file

---
+++ script.js
@@ -0,0 +1,15 @@
+
+// Javascript External File
+
+   alert ("I am from <BODY>. Script code from external file"); 
+
+
+    /*  function myfunction() {
+        var data = document.forms["myForm"]["fname"].value;
+        if (data == "") {
+            alert("Name must be filled out");
+            return false;
+        }
+    } */
+
+  
